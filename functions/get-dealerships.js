5540ad3fa9854734bebfba912a5958bf780da6d2
JanWysienskiWork <jan.wysienski@gmail.com>
2022-06-12 15:51:48 +0000

    this is impossible

---
+++ functions/get-dealerships.js
@@ -0,0 +1,16 @@
+const md5 = require('spark-md5');
+
+function main(params) {
+    return {
+      entries: params.rows.map((row) => { return { 
+          id: row.doc.id,
+          city: row.doc.city,
+          state: row.doc.state,
+          st: row.doc.st,
+          address: row.doc.address,
+          zip: row.doc.zip,
+          lat: row.doc.lat,
+          long: row.doc.long,
+      }})
+    };
+  }
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -82,14 +82,13 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    url = "https://e767a744.eu-gb.apigw.appdomain.cloud/api/api/dealership"
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
-
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+#def get_dealer_details(request, dealer_id):
+#    if request.method == "GET":
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
